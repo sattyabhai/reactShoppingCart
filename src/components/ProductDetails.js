24cbab0de3edea2401a3cef8b07b057baa29985f
sattyabhai <satyajitgh143@gmail.com>
2024-03-26 20:01:33 +0530

    before checkout

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -5,6 +5,9 @@ import { useParams } from 'react-router-dom';
 function ProductDetails() {
     const { id } = useParams();
     const [productDetails, setproductDetails] = useState([]);
+    const [cartButtonText, setcartButtonText] = useState('Add to cart');
+    const [cartButtonStatus, setcartButtonStatus] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
     const productApi = process.env.REACT_APP_PRODUCT_API_URL;
     useEffect(() => {
         const fetchProductDetails = () => {
@@ -20,25 +23,46 @@ function ProductDetails() {
         
         fetchProductDetails();
     }, [productApi,id]); // Include productApi in the dependency array if it can change
-    console.log(productDetails);
+
+    let addToCart = () =>{
+             const isItemInCart = cartItems.find((cartItem) => cartItem.id === productDetails.id);
+             if (isItemInCart) {
+                setCartItems(
+                    cartItems.map((cartItem) => // if the item is already in the cart, increase the quantity of the item
+                    cartItem.id === productDetails.id
+                        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                        : cartItem // otherwise, return the cart item
+                    )
+                );
+                } else {
+                setCartItems([...cartItems, { ...productDetails, quantity: 1 }]); // if the item is not in the cart, add the item to the cart
+                }
+                setcartButtonText("Added to cart");
+                setcartButtonStatus(true)
+                sessionStorage.setItem("cartItems", JSON.stringify(cartItems));
+
+            // console.log(cartItems)
+    }
+
+
+
     return (
       <>
 
             <div className="row">
-                {/* { */}
-                    {/* // productDetails.map((product) => ( */}
-                        <div className="col-sm-12" key={productDetails.id}>
-                            <div className="card">
-                                <img className="card-img-top" style={{width: "500px"}} src={productDetails.image} alt={productDetails.title}/>
-                                <div className="card-body">
-                                    <h5 className="card-title">{productDetails.title}</h5>
-                                    <span className="card-title">${productDetails.price}</span>
-                                    <p className="card-text">{productDetails.description}</p>
-                                </div>
-                                </div>
+                <div className="col-sm-12" key={productDetails.id}>
+                    <div className="card">
+                        <img className="card-img-top" style={{width: "500px"}} src={productDetails.image} alt={productDetails.title}/>
+                        <div className="card-body">
+                            <h5 className="card-title">{productDetails.title}</h5>
+                            <span className="card-title">${productDetails.price}</span>
+                            <p className="card-text">{productDetails.description}</p>
+                            <button className="add-to-cart" onClick={addToCart}>{cartButtonText}</button>
+                            {cartButtonStatus && <a href="/">Checkout</a>}
                         </div>
-                    {/* ))
-                } */}
+                        </div>
+                </div>
+               
             </div>      
       
       </>
